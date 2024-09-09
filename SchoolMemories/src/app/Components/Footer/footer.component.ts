ef12a8038cc5f2e2e4b1b3c28cdb80b6639c5a10
rohanpatel4846 <rohanpatel4846@gmail.com>
2024-09-09 22:13:25 +0530

    .

--- SchoolMemories/src/app/Components/Footer/footer.component.ts
+++ SchoolMemories/src/app/Components/Footer/footer.component.ts
@@ -1,12 +1,66 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NzCollapseModule } from 'ng-zorro-antd/collapse';
+import { RouterModule } from '@angular/router';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    NzCollapseModule,
+    CommonModule,
+    RouterModule,
+    NzGridModule,
+    NzIconModule
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+  links: any = [
+    {
+      "text": "Random Password Generator",
+      "routerLink": "https://gamble4846.github.io/AngularTools/RandomPasswordGenerator",
+      "fullLink": true
+    },
+  ]
 
+  linksPart1: any = [];
+  linksPart2: any = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    for (var i = 0; i < this.links.length; i++) {
+      if ((i + 2) % 2 == 0) {
+        this.linksPart1.push(this.links[i]);
+      }
+      else {
+        this.linksPart2.push(this.links[i]);
+      }
+    }
+  }
+
+  SocialClicked(social: string) {
+    let url = "";
+    switch (social) {
+      case 'instagram':
+        url = "https://www.instagram.com/gamble4846/";
+        break;
+      case 'github':
+        url = "https://github.com/gamble4846";
+        break;
+      case 'linkedin':
+        url = "https://www.linkedin.com/in/rohanpatel4846/";
+        break;
+      case 'reddit':
+        url = "https://www.reddit.com/user/gamble4846";
+        break;
+      default:
+        break;
+    }
+    window.open(url, '_blank')!.focus();
+  }
 }
